d76b1731836a73e3615a5a3799296368626ecd32
Jeff McMillan <jeff.d.mcmillan@outlook.com>
2018-02-02 16:57:00 -0700

    Removed duplicated use-package chunk

--- packages-config.el
+++ packages-config.el
@@ -1,20 +1,5 @@
 ;; Package config
 
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(eval-when-compile
-  (require 'use-package))
-
 ;; the package manager
 (require 'package)
 (setq
